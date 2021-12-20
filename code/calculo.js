b6ffa5394f0d31a45be70e312c17ffe4a389648c
Mario Gabriel PC <mario.contate.me@gmail.com>
2021-12-20 10:54:52 -0300

    calculo funcionando com name, não unir

--- code/calculo.js
+++ code/calculo.js
@@ -1,43 +1,46 @@
-const heigthInput = document.querySelector("#height").value;
-const weigthInput = document.querySelector("#weight").value;
+//Selecionar o elemento da dom
+
+const nameInput = document.querySelector("#name");
+const heigthInput = document.querySelector("#heigth");
+const weigthInput = document.querySelector("#weigth");
 const submitButton = document.querySelector("#submit-button");
 const messageImc = document.querySelector(".message");
 
+// Adicionar Evento no click do button
 submitButton.addEventListener("click", (event) => {
   event.preventDefault();
+  const nameValue = nameInput.value;
   const heigthValue = heigthInput.value / 100;
   const weigthValue = weigthInput.value;
 
-  if (heigthValue === "" || weigthValue === ""){
-    alert("preencha todos os campos");
+  if (nameValue === "" || heigthValue === "" || weigthValue === "") {
+    alert("Preencha todos os campos");
     return;
   }
 
-  function calculaImc(weight, height) {
-    const data = weight / (height * height);
+  function calculaImc(weigth, heigth) {
+    const data = weigth / (heigth * heigth);
     const total = data.toFixed(2);
 
     let message;
 
     if (data < 18.5) {
-      message = `Seu imc é de ${total} você está abaixo do peso!`;
+      message = `${nameValue} seu imc é de ${total} você está abaixo do peso!`;
     } else if (data >= 18.5 && data <= 24.99) {
-      message = `Seu imc é de ${total} vocÊ esrtá com o peso saldavel`;
+      message = `${nameValue} seu imc é de ${total} você está com o peso saudável!`;
     } else if (data >= 25 && data <= 29.99) {
-      message = `Seu imc é de ${total} você está com sobrepeso!`;
+      message = `${nameValue} seu imc é de ${total} você está com sobrepeso!`;
     } else if (data >= 30 && data <= 39.99) {
-      message = `Seu imc é de ${total} você está acima do peso`;
+      message = `${nameValue} seu imc é de ${total} você está obeso!`;
     } else if (data > 40) {
-      message = `Seu imc é de ${total} Cuidado você esta com obesidade grave!`;
+      message = `${nameValue} seu imc é de ${total} você está com obesidade grave!`;
     }
 
-
-    messageImc.innerHTML = console.log(message);
+    messageImc.innerHTML = message;
+    nameInput.value = "";
     heigthInput.value = "";
     weigthInput.value = "";
     return;
   }
   calculaImc(weigthValue, heigthValue);
 });
-
-
